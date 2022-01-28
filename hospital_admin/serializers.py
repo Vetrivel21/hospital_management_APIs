5d71c7b70b08f4c3553dc34f5e0a8edf3cefa0ca
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-01-28 22:08:07 +0530

    Update serializers.py

--- hospital_admin/serializers.py
+++ hospital_admin/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from .models import doctor
+from .models import patient
 
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = doctor
+        fields = "__all__"
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = patient
+        fields = "__all__"
