6feb76f3a08f9e1a33cca761d8184e04d02d8773
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-01-28 22:06:23 +0530

    Update admin.py

--- hospital_admin/admin.py
+++ hospital_admin/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import doctor
+from .models import patient
+
+admin.site.register(doctor)
+admin.site.register(patient)
 
