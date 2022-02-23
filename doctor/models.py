462f8b9757823bd21f0fc9972d36a008fa4b4577
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-02-23 22:46:20 +0530

    Update models.py

--- doctor/models.py
+++ doctor/models.py
@@ -1,53 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
-from django.contrib.auth.models import AbstractUser
-#from django.utils import timezone
+
+
 # Create your models here.
 
-class doctor_account(AbstractBaseUser):
-    username = models.CharField(max_length=200, unique=True)
-    password = models.CharField(max_length=300, default='')
-    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    address = models.TextField(blank=True)
-    mobile_no = models.IntegerField(default=0, blank=True)
-    specialization = models.CharField(max_length=60, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True)
-    date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True, null=True)
-    last_login = models.DateTimeField(verbose_name="last login", auto_now_add=True, null=True)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    #is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    #hide_email = models.BooleanField(default=True)
-
-
-    objects = UserManager()
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'address', 'password', 'mobile_no', 'specialization']
-
-    '''def __str__(self):
-        return self.username
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return True'''
-
-    def create_user(self, username, email, password=None):
-        if not email:
-            raise ValueError("Enter a valid email address.")
-        if not username:
-            raise ValueError("Enter a valid email username.")
-        user = self.model(
-            email=self.normalize_email(email),
-            username = username,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
+class account(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100, blank=True)
 
 
 
