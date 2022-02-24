52dfc3515bc3423c3a31582af0765b3d71c3033b
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-02-24 23:03:27 +0530

    Update views.py

--- doctor/views.py
+++ doctor/views.py
@@ -4,23 +4,42 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import Register_apiSerializer
+from .serializers import UserSerializer, Register_apiSerializer
 from django.contrib.auth import login
 from rest_framework import permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
-from .models import doctor_account
+from .models import account
 from rest_framework import status
+from rest_framework.views import APIView
+from django.core.mail import send_mail
+
+class user_information(APIView):
+    def post(self, request):
+        send_mail(
+            'Doctor details',
+            'Name: Sivan, email: sivan@gmail.com, address: 524/1, Jawahar Bazaar, Karur., mobileno: 2134567854, specialization: M.D.',
+            'fromkavithaswaminathan@gmail.com',
+            ['tovetrisenthilmkce@gmail.com'],
+            fail_silently=False,
+        )
+
 
 class RegisterAPI(generics.GenericAPIView):
-
     def post(self, request):
-        doctor_details = doctor_account.objects.all()
+        doctor_details = account.objects.all()
         serializer = Register_apiSerializer(doctor_details, many=True)
         serializer = Register_apiSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+            send_mail(
+                'Doctor details',
+                'username, password',
+                'fromvetrisenthilmkce@gmail.com',
+                ['tokavithaswaminathan433@gmail.com'],
+                fail_silently=False,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
                 #"user": UserSerializer(user, context=self.get_serializer_context()).data,
                 #"token": AuthToken.objects.create(user)[1],
@@ -32,12 +51,10 @@ class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format=None):
+        doctor_details = account.objects.all()
+        serializer = UserSerializer(doctor_details, many=True)
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         login(request, user)
         return super(LoginAPI, self).post(request, format=None)
-
-
-
-# Create your views here.
