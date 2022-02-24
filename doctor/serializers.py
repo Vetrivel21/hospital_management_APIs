1885dfaea344bcd0b70316ad8cd295aeab921284
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-02-24 23:04:46 +0530

    Update serializers.py

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -1,21 +1,26 @@
 from rest_framework import serializers, validators
 from django.contrib.auth.models import User
-from .models import doctor_account
+from .models import account
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = doctor_account
+        model = account
         fields = "__all__"
+
+    def create(self, validated_data):
+            user = account.objects.create_user(validated_data['username'], validated_data['password'])
+
+            return user
 
 # Register Serializer
 class Register_apiSerializer(serializers.ModelSerializer):
     class Meta:
-        model = doctor_account
+        model = account
         fields = "__all__"
         #fields = ('id', 'username', 'email')
-        extra_kwargs = {'password': {'write_only': True}}
+        '''extra_kwargs = {'password': {'write_only': True}}'''
 
-    def create(self, validated_data):
-        user = doctor_account.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+    '''def create(self, validated_data):
+        user = account.objects.create_user(self.validated_data['username'], password=self.validated_data['password'])
 
-        return user
+        return user'''
