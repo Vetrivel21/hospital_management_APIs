7e74b3409d3902abe17962a9748d98b1186f27c8
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-03-09 22:07:10 +0530

    Update models.py

--- hospital_admin/models.py
+++ hospital_admin/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+'''class SoftDeleteManager(models.Manager):
+
+    def get_queryset(self, *args, **kwargs):
+        return super(SoftDeleteManager, self).get_queryset(*args, **kwargs).filter(is_active=True)
+
+    def everything(self, *args, **kwargs):
+        return super(SoftDeleteManager, self).get_queryset(*args, **kwargs)'''
 
 class doctor(models.Model):
     name = models.CharField(max_length=200, blank=True)
@@ -10,6 +18,11 @@ class doctor(models.Model):
     specialization = models.CharField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now_add=True, null=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
 
 class patient(models.Model):
     name = models.CharField(max_length=200, blank=True)
@@ -19,10 +32,14 @@ class patient(models.Model):
     symptoms = models.CharField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now_add=True, null=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
 
     def __str__(self):
         return self.name
-
 
 
 
