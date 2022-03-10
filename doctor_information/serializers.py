e24040533ceba08dc0ecfac0908faafaa4d04c1d
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-03-10 22:41:37 +0530

    Update serializers.py

--- doctor_information/serializers.py
+++ doctor_information/serializers.py
@@ -1,34 +1,11 @@
-from rest_framework import serializers, validators
+from rest_framework import serializers
+from .models import doctor_details
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
-from .models import account
-from .models import user_details
-# User Serializer
-class UserSerializer(serializers.ModelSerializer):
+
+class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = user_details
+        model = doctor_details
         fields = "__all__"
 
 
-    '''def create(self, validated_data):
-            user = User.objects.create_user(validated_data['username'], validated_data['password'])
 
-            return user'''
-
-# Register Serializer
-class Register_apiSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = account
-        fields = "__all__"
-        #fields = ('id', 'username', 'email')
-        '''extra_kwargs = {'password': {'write_only': True}}'''
-
-    '''def create(self, validated_data):
-        user = account.objects.create_user(self.validated_data['username'], password=self.validated_data['password'])
-
-        return user'''
-
-class LoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = account
-        fields = "__all__"
