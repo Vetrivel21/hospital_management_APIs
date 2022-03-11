1bd24eca2ab0edc66c571019eda1d36404e91620
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-03-11 23:11:09 +0530

    Update views.py

--- doctor_information/views.py
+++ doctor_information/views.py
@@ -1,14 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import doctor_details
 from .serializers import RegisterSerializer
 from rest_framework import status
+from rest_framework import generics, permissions, mixins
+from hospital_admin.models import doctor
 from knox.models import AuthToken
 from django.contrib.auth import login
 from rest_framework import permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
 from django.core.mail import send_mail
+from rest_framework.permissions import IsAuthenticated
+from .serializers import LoginSerializer
+
 
 class RegisterAPI(APIView):
     def post(self, request):
@@ -18,10 +22,24 @@ class RegisterAPI(APIView):
             send_mail(
                 'Doctor credentials',
                 f'{serializer.data}',
-                'fromsenthil123@gmail.com',
+                'from{email}',
                 ['tovetrisenthilmkce@gmail.com'],
                 fail_silently=False,
             )
+            '''return Response({
+                "user": UserSerializer(user, context=self.get_serializer_context()).data,
+                "message": "User Created Successfully.  Now perform Login to get your token",
+            })'''
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPI(APIView):
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
