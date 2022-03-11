0eff8c870614f9fc9cdce431486f4bf7c8e9c992
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-03-11 22:56:35 +0530

    Update urls.py

--- doctor_information/urls.py
+++ doctor_information/urls.py
@@ -1,10 +1,13 @@
 from .views import RegisterAPI
 from django.urls import path
 from knox import views as knox_views
+from .views import LoginAPI
 
 
 
 urlpatterns = [
     path('doctor/register/', RegisterAPI.as_view(), name='register'),
-   
+    path('doctor/login/', LoginAPI.as_view(), name ='login'),
+
+
 ]
