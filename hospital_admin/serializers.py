e59b9376593b93093d427ba3f64c2cf6c0c653a9
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-03-15 20:57:55 +0530

    Update serializers.py

--- hospital_admin/serializers.py
+++ hospital_admin/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import doctor
 from .models import patient
+from rest_framework import serializers, validators
+from django.contrib.auth.models import User
 
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +13,19 @@ class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = patient
         fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+
+        return user
